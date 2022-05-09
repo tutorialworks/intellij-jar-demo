1c4ef231551d55255722393efb8f1ca34943e708
Tom Donohue <monodot@gmail.com>
2022-05-09 09:35:59 +0100

    first commit

--- maven-jar/src/main/java/com/tutorialworks/Book.java
+++ maven-jar/src/main/java/com/tutorialworks/Book.java
@@ -1,2 +1,24 @@
+package com.tutorialworks;
+
 public class Book {
+
+    private String author;
+    private String title;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
 }
